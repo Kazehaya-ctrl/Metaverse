d24407f42a43df97759fc44f6f74ad9bed7dc344
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-01-01 20:02:38 +0530

    upt: initializing the project with phaserjs

---
+++ app/game.js
@@ -0,0 +1,5 @@
+import { useEffect } from "react";
+import Phaser from "phaser";
+import config from "../phaser/config";
+
+var game = new Phaser.Game(config);
---
+++ phaser/config.js
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+
+const config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: { y: 300 },
+			debug: false,
+		},
+	},
+	scene: [],
+};
+
+export default config;
